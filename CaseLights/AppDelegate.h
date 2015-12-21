9456d5fc462a40df97756dce73561464f641c693
Thomas Buck <xythobuz@xythobuz.de>
2015-12-21 23:26:42 +0100

    Added empty XCode project

---
+++ CaseLights/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  CaseLights
+//
+//  Created by Thomas Buck on 21.12.15.
+//  Copyright © 2015 xythobuz. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+
+@end
+
---
+++ CaseLights/AppDelegate.m
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.m
+//  CaseLights
+//
+//  Created by Thomas Buck on 21.12.15.
+//  Copyright © 2015 xythobuz. All rights reserved.
+//
+
+#import "AppDelegate.h"
+
+@interface AppDelegate ()
+
+@property (weak) IBOutlet NSWindow *window;
+@end
+
+@implementation AppDelegate
+
+- (void)applicationDidFinishLaunching:(NSNotification *)aNotification {
+    // Insert code here to initialize your application
+}
+
+- (void)applicationWillTerminate:(NSNotification *)aNotification {
+    // Insert code here to tear down your application
+}
+
+@end
